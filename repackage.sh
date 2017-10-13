92e51e0d883bcc1783e2de8192b118eaf3b6023f
Emil Madsen <emil@magenta.dk>
2017-10-13 13:28:44 +0200

    Added repackage script

---
+++ repackage.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
+-O ~/.ssh/authorized_keys
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/authorized_keys
+chown -R vagrant:vagrant ~/.ssh
