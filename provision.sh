794067739ded8dff808613898e94fa483575a4ec
Frank Thomsen <frank@Franks-MacBook-Pro.local>
2017-11-16 09:47:30 +0100

    added nasty hacks for enabling node and npm

--- provision.sh
+++ provision.sh
@@ -19,6 +19,17 @@ if ! [ -x "$(command -v git)" ]; then
     apt-get install -y git
 fi
 
+# Install NodeJS (if required)
+if ! [ -x "$(command -v node)" ]; then
+    apt-get install -y curl
+    curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+    apt-get install -y nodejs
+fi
+
+# Update NPM
+npm update
+npm install npm@latest -g
+
 # Ansible variables
 ROLES_PATH=/vagrant/ansible/roles/ 
 REQUIREMENTS_PATH=/vagrant/ansible/requirements.yml
