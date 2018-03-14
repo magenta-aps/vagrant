27d1c3d90cad40dc7d4a92da27762774a767a143
Andreas Kring <andreas@magenta.dk>
2018-03-14 17:12:35 +0100

    Added Ubuntu Xenial Ansible ppa - now installing Ansible 2.4 instead of version 2.2

--- provision.sh
+++ provision.sh
@@ -11,6 +11,10 @@ fi
 
 # Install Ansible (if required)
 if ! [ -x "$(command -v ansible)" ]; then
+    echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu xenial main" \
+    >> /etc/apt/sources.list
+    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+    apt-get update
     apt-get install -y ansible
 fi
 
