c64f30347942dc14660b0418537ca4b30cc83644
Emil Madsen <emil@magenta.dk>
2018-05-03 14:03:55 +0200

    Default to libvirt

--- libvirt_clean.sh
+++ libvirt_clean.sh
@@ -6,4 +6,5 @@ MACHINE_ID=$(sudo virsh list --all | grep "$NAME" | cut -f2 -d' ')
 # Destroy everything
 sudo virsh destroy $MACHINE_ID
 sudo virsh vol-delete --pool default ${NAME}.img
+sudo vagrant destroy -f 2>/dev/null
 sudo vagrant destroy -f
